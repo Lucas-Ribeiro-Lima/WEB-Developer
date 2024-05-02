71a9f7269536276faf2e63e4eef027d0d6e98ade
lucas.lima <lucasribeirolima974@gmail.com>
2024-05-02 14:18:43 -0300

    feat: add functions to control errors on form

--- estudo/projetos/imc-calc/src/javascript/script.js
+++ estudo/projetos/imc-calc/src/javascript/script.js
@@ -1,8 +1,18 @@
+/**
+ * Calculate the IMC based on peso and altura
+ * @param {Number} peso 
+ * @param {Number} altura 
+ * @returns  The calculated IMC and the respective status
+ */
 function calculaIMC(peso, altura){
   const imc = peso / (Math.pow(altura, 2))
   const string = `IMC: ${imc.toFixed(1)}`
 
   let resultado
+
+  if (Number.isNaN(imc) || imc === undefined || imc === Infinity){
+    return resultado = `Valores inválidos`
+  }
 
   if (imc > 0 && imc < 18.5) {
     resultado = `${string} Status: (Abaixo do peso)`
@@ -23,48 +33,67 @@ function calculaIMC(peso, altura){
     resultado = `${string} Status: (Obesidade grau 3)`
   }
 
-  if (Number.isNaN(imc) || imc === undefined || imc === Infinity){
-    resultado = `Valores inválidos`
-  }
-
   return resultado
+}
+
+/**
+ * 
+ * Controls the errors on the form.
+ * @param {Array} formErrors
+ * @returns return True if the form have errors
+ * 
+ */
+function formErrors(values) {
+const errorMsg = document.querySelectorAll('span.error')
+const inputMsg = document.querySelectorAll('.formulario input')
+
+const errors = []
+values.forEach((value, index) => {
+  if (!value) {
+    inputMsg[index].classList.add('error', 'ativo')
+    errorMsg[index].classList.add('ativo')
+    errors.push(!value)
+  } 
+  else {
+    inputMsg[index].classList.remove('error', 'ativo')
+    errorMsg[index].classList.remove('ativo')
+    errors.push(value)
+  }
+})
+  return errors.includes(true)
+}
+
+/**
+ * 
+ * Control the form submit
+ * @param { Number } peso 
+ * @param { Number } altura 
+ * @returns void
+ * 
+ */
+function formControl(peso, altura) {
+  const resultadoDiv = document.querySelector('.resultado')
+
+  resultadoDiv.classList.remove('ativo')
+
+  const errors = formErrors([peso, altura])
+
+  if (!errors) {
+    const imc = calculaIMC(peso, altura)
+
+    resultadoDiv.classList.add('ativo')
+    resultadoDiv.innerHTML = imc
+  }
 }
 
 !function handleSubmit() {
   const form = document.querySelector('.formulario')
-  const resultado = document.querySelector('.resultado')
-  const errorMsg = document.querySelectorAll('.error')
-  const inputErrorMsg = document.querySelectorAll('.formulario input')
-
   form.addEventListener('submit', (event) => {
     event.preventDefault()
 
     const peso = Number(event.target.peso.value)
     const altura = Number(event.target.altura.value.replace(',', '.'))
 
-    if (!peso) {
-      inputErrorMsg[0].classList.add('error', 'ativo')
-      errorMsg[0].classList.add('ativo')
-      resultado.classList.remove('ativo')
-    } else {
-      inputErrorMsg[0].classList.remove('error', 'ativo')
-      errorMsg[0].classList.remove('ativo')
-    }
-    
-    if (!altura) {
-      errorMsg[1].classList.add('ativo')
-      inputErrorMsg[1].classList.add('error', 'ativo')
-      resultado.classList.remove('ativo')
-    } else {
-      errorMsg[1].classList.remove('ativo')
-      inputErrorMsg[1].classList.remove('error', 'ativo')
-    }
-
-    if (peso && altura) {
-      const imc = calculaIMC(peso, altura)
-  
-      resultado.classList.add('ativo')
-      resultado.innerHTML = imc
-    }
+    formControl(peso, altura)
   })
 }()
