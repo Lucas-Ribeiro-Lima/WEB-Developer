d230c25f7c29585f9cb5149b14a06247a9e54576
lucas.lima <lucasribeirolima974@gmail.com>
2024-05-02 16:57:32 -0300

    feat: add getNivelMC function that returns the level

--- estudo/projetos/imc-calc/src/javascript/script.js
+++ estudo/projetos/imc-calc/src/javascript/script.js
@@ -1,3 +1,15 @@
+!function handleSubmit() {
+  const form = document.querySelector('.formulario')
+  form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const peso = Number(event.target.peso.value)
+    const altura = Number(event.target.altura.value.replace(',', '.'))
+
+    formControl(peso, altura)
+  })
+}()
+
 /**
  * Calculate the IMC based on peso and altura
  * @param {Number} peso 
@@ -6,34 +18,47 @@
  */
 function calculaIMC(peso, altura){
   const imc = peso / (Math.pow(altura, 2))
-  const string = `IMC: ${imc.toFixed(1)}`
-
-  let resultado
+  const imcStr = `IMC: ${imc.toFixed(2)}`
+  const nivel = getNivelIMC(imc)
 
   if (Number.isNaN(imc) || imc === undefined || imc === Infinity){
     return resultado = `Valores inválidos`
   }
 
-  if (imc > 0 && imc < 18.5) {
-    resultado = `${string} Status: (Abaixo do peso)`
-  } 
-  else if (imc >= 18.5 && imc < 25) {
-    resultado = `${string} Status: (Peso normal)`
-  }
-  else if (imc >= 25 && imc < 30) {
-    resultado = `${string} Status: (Sobrepeso)`
-  }
-  else if (imc >= 30 && imc < 35) {
-    resultado = `${string} Status: (Obesidade grau 1)`
-  }
-  else if (imc >= 35 && imc < 40) {
-    resultado = `${string} Status: (Obesidade grau 2)`
-  }
-  else if (imc >= 40) {
-    resultado = `${string} Status: (Obesidade grau 3)`
-  }
+  const resultado = `${imcStr} ${nivel}`
 
   return resultado
+}
+
+/**
+ * 
+ * @param {Number} imc 
+ * @returns The string with the correct IMC level string
+ */
+function getNivelIMC(imc) {
+  const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obseidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3']
+  let string
+
+  if (imc > 0 && imc < 18.5) {
+    string = `Status: ${nivel[0]}`
+  } 
+  else if (imc >= 18.5 && imc < 25) {
+    string = `Status: ${nivel[1]}`
+  }
+  else if (imc >= 25 && imc < 30) {
+    string = `Status: ${nivel[2]}`
+  }
+  else if (imc >= 30 && imc < 35) {
+    string = `Status: ${nivel[3]}`
+  }
+  else if (imc >= 35 && imc < 40) {
+    string = `Status: ${nivel[4]}`
+  }
+  else if (imc >= 40) {
+    string = `Status: ${nivel[5]}`
+  }
+
+  return string
 }
 
 /**
@@ -86,14 +111,3 @@ function formControl(peso, altura) {
   }
 }
 
-!function handleSubmit() {
-  const form = document.querySelector('.formulario')
-  form.addEventListener('submit', (event) => {
-    event.preventDefault()
-
-    const peso = Number(event.target.peso.value)
-    const altura = Number(event.target.altura.value.replace(',', '.'))
-
-    formControl(peso, altura)
-  })
-}()
